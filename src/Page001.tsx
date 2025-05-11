937b7289daf7212e64ab0bcf0bd98c34e974510f
Johblanc <johannblanc@hotmail.fr>
2025-05-11 20:01:50 +0200

    feat : page 001

---
+++ src/Page001.tsx
@@ -0,0 +1,92 @@
+import { useContext } from "react";
+import "./App.css";
+import { HorizontalBarGraph } from "./Components/HorizontalBarGraph";
+import { VerticalBarGraph } from "./Components/VerticalBarGraph";
+import { CircularGraph } from "./Components/CircularGraph";
+import { LignesContext } from "./Context/Lignes/LignesContext";
+import { SizeContext } from "./Context/Size/SizeContext";
+
+export function Page001() {
+  const { lignesTypes, lignesRegions, lignesSpeed } = useContext(LignesContext);
+  const { width } = useContext(SizeContext);
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          gap: "40px",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+      >
+        <div>
+          <h2>Ligne par type</h2>
+          <CircularGraph
+            id={"lignes-type"}
+            data={lignesTypes}
+            width={Math.min(400, width - 50)}
+            height={550}
+            groupBy={"type_ligne"}
+            size={0.65}
+          />
+        </div>
+        <div>
+          <h2>Ligne par type</h2>
+          <HorizontalBarGraph
+            id={"lignes-region"}
+            data={lignesRegions}
+            width={Math.min(500, width - 50)}
+            height={600}
+            legendWidth={200}
+            barColor={"red"}
+            groupBy={"region"}
+          />
+        </div>
+      </div>
+      <div>
+        <h2>Ligne par Vitesse</h2>
+        <VerticalBarGraph
+          id={"lignes-speed"}
+          data={lignesSpeed}
+          width={width - 50}
+          height={300}
+          barColor={"red"}
+          groupBy={"v_max"}
+          legendHeight={30}
+        />
+      </div>
+      <div
+        style={{
+          marginTop: "20px",
+          maxHeight: "400px",
+          overflowY: "auto",
+        }}
+      >
+        <table>
+          <thead
+            style={{
+              position: "sticky",
+              top: 0,
+              backgroundColor: "var(--var-color-app-bg)",
+            }}
+          >
+            <tr>
+              <th>Code Ligne</th>
+              <th>Nom Ligne</th>
+              <th>Type Ligne</th>
+            </tr>
+          </thead>
+          <tbody>
+            {lignesTypes.map((ligne) => (
+              <tr key={ligne.code_ligne}>
+                <td>{ligne.code_ligne}</td>
+                <td>{ligne.lib_ligne}</td>
+                <td>{ligne.type_ligne}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+}
